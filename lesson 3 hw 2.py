5eb561b23e394e03101283c94192c5c6baedad05
DrS <drsovcharov@gmail.com>
2021-06-23 21:22:07 +0200

    lesson 3

---
+++ lesson 3 hw 2.py
@@ -0,0 +1,4 @@
+def my_func(name, surname, byear, city, email, phone):
+     print(fname, surname, byear, city, email, phone)
+
+ my_func(name= 'stas', surname='ovcharov', byear=1994, city='Kld', email='drs....@gmail.com', phone='+7911......')
